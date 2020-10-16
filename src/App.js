43a2d7a73bdd02a7d81a9ddf3d2da17e9f48abdc
Pitunetix <griusech89@gmail.com>
2020-10-15 22:35:38 -0300

    Creo el proyecto para ser usado con Redux y agrego material UI

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Sidebar from './Sidebar/Sidebar'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      {/* SIDEBAR */}
+      <Sidebar /> 
+
+      {/* CHATS */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Sidebar/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Sidebar.css';
+
+const Sidebar = () => {
+    return (
+        <div>
+            SOY EL SIDEBAR
+
+            <div className="sidebar-top">
+                <h3>Guille Developer</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
