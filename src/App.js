bda1718e0b84bdd1a0b5a65c91dbc071bdeef2f6
Pitunetix <griusech89@gmail.com>
2020-10-17 17:05:35 -0300

    Divido los componentes para login

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import Sidebar from './Sidebar/Sidebar'
+import { useSelector } from 'react-redux'
 import './App.css';
 import Chat from './Chat/Chat';
 import Logos from './Logos/Logos';
+import { selectUser } from './features/userSlice'
 
 function App() {
+
+  const user = useSelector(selectUser)
+
   return (
     <div className="app">
 
-      {/* <Logos /> */}
+      {user ? (
+        <>
+          {/* <Logos /> */}
+    
+          <Sidebar /> 
+    
+          <Chat />
+        </>
+      ) : (
+        <h1>ACA DEBO COLOCARL EL LOGIN</h1>
+      )}
 
-      <Sidebar /> 
-
-      <Chat />
 
     </div>
   );
