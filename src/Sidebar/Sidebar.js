5d2cbfff10426500de50e1f3370f978c9105976e
Pitunetix <griusech89@gmail.com>
2020-10-16 21:38:07 -0300

    Agrego el componente de los canales

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Sidebar.css';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
+import SidebarChannel from '../SidebarChannel/SidebarChannel';
 
 const Sidebar = () => {
     return (
@@ -20,6 +21,12 @@ const Sidebar = () => {
                     </div>
                     <AddIcon className="sidebar-addChannel" />
                 </div>
+                <div className="sidebar-channelList">
+                    <SidebarChannel />
+                    <SidebarChannel />
+                    <SidebarChannel />
+                    <SidebarChannel />
+                </div>    
             </div>
         </div>
     )
---
+++ src/SidebarChannel/SidebarChannel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SidebarChannel.css'
+
+const SidebarChannel = () => {
+    return (
+        <div className="sidebarChannel">
+           <h4><span className="sidebarChannel-hash">#</span>Youtube</h4>
+        </div>
+    )
+}
+
+export default SidebarChannel
