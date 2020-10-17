b894ad6f9af727c856d51c83620d1f7502347b7a
Pitunetix <griusech89@gmail.com>
2020-10-17 00:10:13 -0300

    Diseño el form y le doy estilo para el envio de los mensajes

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ChatHeader from '../ChatHeader/ChatHeader'
 import './Chat.css'
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
+import GifIcon from '@material-ui/icons/Gif';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
 
 
 const Chat = () => {
@@ -13,7 +17,21 @@ const Chat = () => {
             </div>
 
             <div className="chat-input">
+                <AddCircleIcon fontSize="medium" />
                 
+                <form action="">
+                    <input placeholder="Enviar mensaje" />                    
+                    <button 
+                    className="chat-inputButton"
+                    type="submit">
+
+                    </button>
+                </form>
+                <div className="chat-inputIcon">
+                    <CardGiftcardIcon fontSize="medium"/>
+                    <GifIcon fontSize="medium"/>
+                    <EmojiEmotionsIcon fontSize="medium"/>
+                </div>
             </div>
 
         </div>
