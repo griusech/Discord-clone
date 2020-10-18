b62ad427f518e64a0c19b04e546cece3d941be6e
Pitunetix <griusech89@gmail.com>
2020-10-17 23:34:14 -0300

    Creo la funcion de enviar los mensajes

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ChatHeader from '../ChatHeader/ChatHeader'
 import Message from '../Message/Message'
 import './Chat.css'
@@ -9,6 +9,8 @@ import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
 import { useSelector } from 'react-redux';
 import { selectChannelId, selectChannelName } from '../features/appSlice';
 import { selectUser } from '../features/userSlice';
+import db from '../firebase';
+import firebase from 'firebase'
 
 const Chat = () => {
 
@@ -19,6 +21,36 @@ const Chat = () => {
     const [ input, setInput ] = useState('')
     const [ messages, setMessages ] = useState([])
 
+    useEffect(() => {
+        if(channelId) {
+            db
+            .collection('channels')
+            .doc(channelId)
+            .collection('msg')
+            .orderBy('timestamp', 'asc')
+            .onSnapshot((snapshot) => (
+                setMessages(snapshot.docs.map(doc => doc.data()))
+            ))
+        }
+    }, [channelId])
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+
+       
+            db.collection('channels')
+            .doc(channelId)
+            .collection('msg')
+            .add({
+                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                message: input,
+                user: user,
+            })
+
+        setInput('')
+        
+    }
+
     return (
         <div className="chat">
             <ChatHeader 
@@ -26,24 +58,35 @@ const Chat = () => {
             />
 
             <div className="chat-messages">
-                {messages.map(msg => (
-                    <Message />
-                ))}
+                {
+                    messages.map(mssg => (
+                        <Message 
+                        key={mssg.message}
+                        timestamp={mssg.timestamp}
+                        message={mssg.message}
+                        user={user}
+                        />
+                    ))
+                       
+                    
+                }
 
             </div>
 
             <div className="chat-input">
                 <AddCircleIcon fontSize="medium" />
                 
-                <form action="">
+                <form>
                     <input 
                     value={input}
                     disabled={!channelId}
                     onChange={e => setInput(e.target.value)}
                     placeholder={channelId ? `Escribir en # ${channelName}` : 'Seleccione un Canal'} />                    
-                    <button 
+                    <button
                     className="chat-inputButton"
-                    type="submit">
+                    type="submit"
+                    onClick={sendMessage}
+                    >
 
                     </button>
                 </form>
--- src/Message/Message.js
+++ src/Message/Message.js
@@ -2,13 +2,13 @@ import React from 'react'
 import './Message.css'
 import { Avatar } from '@material-ui/core'
 
-const Message = () => {
+const Message = ({message, user, timestamp}) => {
     return (
         <div className="message">
-            <Avatar />
+            <Avatar src={user.photo}/>
             <div className="message-info">
-                <h4>Pitunetix<span className="message-timestamp">Just Now</span></h4>
-            <p>Mensaje</p>
+                <h4>{user.displayName}<span className="message-timestamp">{new Date(timestamp?.toDate()).toUTCString()}</span></h4>
+            <p>{message}</p>
             </div>
         </div>
     )
